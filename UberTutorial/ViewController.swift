7bd13d3c198084c15be67bff627f79d267e1a8da
Chris Kong <christopher.j.kong@gmail.com>
2015-09-10 21:51:24 +0800

    add hello world comment

--- UberTutorial/ViewController.swift
+++ UberTutorial/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+// hello world
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
