580dc67a8376674900774035b0a3738edb433830
Chris Kong <christopher.j.kong@gmail.com>
2015-09-11 00:00:59 +0800

    Configure Sign Up ViewController

--- UberTutorial/ViewController.swift
+++ UberTutorial/ViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController
 {
+  @IBOutlet weak var usernameField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+  @IBOutlet weak var `switch`: UISwitch!
+  @IBOutlet weak var riderLabel: UILabel!
+  @IBOutlet weak var driverLabel: UILabel!
+  @IBOutlet weak var signUpButton: UIButton!
+  @IBOutlet weak var toggleSignUpButton: UIButton!
 
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+
+    
+  }
+  
+  @IBAction func signUpAction(sender: UIButton) {
+    if usernameField.text == "" || passwordField.text == ""
+    {
+      let alert = UIAlertController(title: "Missing Field(s)", message: "Username and password are required", preferredStyle: UIAlertControllerStyle.Alert)
+      alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+      self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+  }
+  
+  @IBAction func toggleSignUpAction(sender: UIButton) {
+  
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+
 }
