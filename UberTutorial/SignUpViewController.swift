cc156f7ea1bb5c8ebfbb578c2b420b88673321ca
Chris Kong <christopher.j.kong@gmail.com>
2015-09-11 00:36:50 +0800

    Add SignUpViewController

--- UberTutorial/SignUpViewController.swift
+++ UberTutorial/SignUpViewController.swift
@@ -1,45 +1,37 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  UberTutorial
 //
-//  Created by Chris Kong on 9/10/15.
+//  Created by Chris Kong on 9/11/15.
 //  Copyright © 2015 Chris Kong. All rights reserved.
 //
 
 import UIKit
-import Parse
 
-class ViewController: UIViewController
+class SignUpViewController: UISearchController
 {
   @IBOutlet weak var usernameField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
-  @IBOutlet weak var `switch`: UISwitch!
+  @IBOutlet weak var accountSwitch: UISwitch!
   @IBOutlet weak var riderLabel: UILabel!
   @IBOutlet weak var driverLabel: UILabel!
   @IBOutlet weak var signUpButton: UIButton!
   @IBOutlet weak var toggleSignUpButton: UIButton!
 
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
 
-    
-  }
-  
-  @IBAction func signUpAction(sender: UIButton) {
+  @IBAction func signUpAction(sender: AnyObject)
+  {
     if usernameField.text == "" || passwordField.text == ""
     {
       let alert = UIAlertController(title: "Missing Field(s)", message: "Username and password are required", preferredStyle: UIAlertControllerStyle.Alert)
       alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
       self.presentViewController(alert, animated: true, completion: nil)
     }
+
+  }
+  @IBAction func toggleSignUpAction(sender: AnyObject) {
+  }
     
-  }
-  
-  @IBAction func toggleSignUpAction(sender: UIButton) {
-  
-  }
 
 
 }
