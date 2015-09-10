eb8c2de8d84f7f7d225466e8597e0cd4324b92f6
Chris Kong <christopher.j.kong@gmail.com>
2015-09-11 01:08:45 +0800

    Modify SignUpViewController, add convenience methods

--- UberTutorial/SignUpViewController.swift
+++ UberTutorial/SignUpViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Parse
 
-class SignUpViewController: UISearchController
+class SignUpViewController: UIViewController, UITextFieldDelegate
 {
   @IBOutlet weak var usernameField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
@@ -17,21 +18,52 @@ class SignUpViewController: UISearchController
   @IBOutlet weak var driverLabel: UILabel!
   @IBOutlet weak var signUpButton: UIButton!
   @IBOutlet weak var toggleSignUpButton: UIButton!
-
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.usernameField.delegate = self
+    self.passwordField.delegate = self
+    
+    // looks for single or multiple taps
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+    view.addGestureRecognizer(tap)
+    
+  }
+  
   @IBAction func signUpAction(sender: AnyObject)
   {
     if usernameField.text == "" || passwordField.text == ""
     {
-      let alert = UIAlertController(title: "Missing Field(s)", message: "Username and password are required", preferredStyle: UIAlertControllerStyle.Alert)
-      alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-      self.presentViewController(alert, animated: true, completion: nil)
+      displayAlert("Missing Field(s)", message: "Username and password are required")
     }
-
   }
+  
   @IBAction func toggleSignUpAction(sender: AnyObject) {
+  
   }
-    
-
+  
+  
+  
+  
+  func displayAlert(title: String, message: String)
+  {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+    self.presentViewController(alert, animated: true, completion: nil)
+  }
+  
+  func textFieldShouldReturn(userText: UITextField) -> Bool
+  {
+    userText.resignFirstResponder()
+    return true
+  }
+  
+  // Calls this function when the tap is recognized
+  func dismissKeyboard()
+  {
+    //Causes the view (or one of its embedded text fields) to resign the first responder status.
+    view.endEditing(true)
+  }
 
 }
