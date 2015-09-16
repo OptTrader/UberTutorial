ee007986b081341d330ee1d6543b68ab24aa770d
Chris Kong <christopher.j.kong@gmail.com>
2015-09-16 17:32:45 +0800

    Added CLLocation to Driver's VC, query & populate TableView

--- UberTutorial/DriverViewController.swift
+++ UberTutorial/DriverViewController.swift
@@ -8,22 +8,124 @@
 
 import UIKit
 import Parse
+import MapKit
 
-class DriverViewController: UITableViewController
+class DriverViewController: UITableViewController, CLLocationManagerDelegate
 {
+  // driver's location
+  
+  var locationManager: CLLocationManager!
+  var latitude: CLLocationDegrees = 0
+  var longitude: CLLocationDegrees = 0
+  
+  // array of riders' data
+  
+  var usernames = [String]()
+  var locations = [CLLocationCoordinate2D]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    locationManager = CLLocationManager()
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingLocation()
+    
+    
 
+  }
 
+  // MARK: - Methods
+  
+  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+  {
+    let location: CLLocationCoordinate2D = manager.location!.coordinate
+    self.latitude = location.latitude
+    self.longitude = location.longitude
+    
+    print("locations = \(location.latitude) \(location.longitude)")
+    
+    // create query function
+    
+    let query = PFQuery(className:"riderRequest")
+    query.whereKey("location", nearGeoPoint: PFGeoPoint(latitude: location.latitude, longitude: location.longitude))
+    query.limit = 10
+    query.findObjectsInBackgroundWithBlock {
+      (objects: [AnyObject]?, error: NSError?) -> Void in
+      
+      if error == nil
+      {
+        print("Successfully retrieved \(objects!.count).")
+        
+        if let objects = objects as? [PFObject]
+        {
+          // remove existing data
+          
+          self.usernames.removeAll()
+          self.locations.removeAll()
+          
+          for object in objects
+          {
+            if let username = object["username"] as? String
+            {
+              self.usernames.append(username)
+            }
+            
+            if let location = object["location"] as? PFGeoPoint
+            {
+              self.locations.append(CLLocationCoordinate2DMake(location.latitude, location.longitude))
+            }
+            
+          }
+          self.tableView.reloadData()
+          
+          print(self.locations)
+          print(self.usernames)
+        }
+      } else {
+        
+        print(error)
+      }
+    }
+    
+//    
+//    
+//    let center = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+//    let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+//    
+//    //self.map.setRegion(region, animated: true)
+//    
+//    // remove annotation
+//    //self.map.removeAnnotations(map.annotations)
+//    
+//    // add pin
+//    let pinLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.latitude, location.longitude)
+//    let objectAnnotation = MKPointAnnotation()
+//    objectAnnotation.coordinate = pinLocation
+//    objectAnnotation.title = "Your Location"
+//    //self.map.addAnnotation(objectAnnotation)
+  }
+  
 
   // MARK: - Table view data source
 
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 0
+    
+    return 1
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
-    return 0
+    
+    return usernames.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+    
+    cell.textLabel?.text = usernames[indexPath.row] + String(locations[indexPath.row].latitude)
+    
+    return cell
   }
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
