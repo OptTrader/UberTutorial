80ceb3bb10bfb3a473a6d9c9581fdad9f1b0a899
Chris Kong <christopher.j.kong@gmail.com>
2015-09-16 12:13:59 +0800

    Added Call Uber Action, save & delete location(s)

--- UberTutorial/RiderViewController.swift
+++ UberTutorial/RiderViewController.swift
@@ -13,8 +13,12 @@ import MapKit
 class RiderViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
 {
   @IBOutlet weak var map: MKMapView!
+  @IBOutlet weak var callUberButton: UIButton!
   
   var locationManager: CLLocationManager!
+  var latitude: CLLocationDegrees = 0
+  var longitude: CLLocationDegrees = 0
+  var riderRequestActive = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,12 +32,71 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, MKMapVie
   
   @IBAction func callUber(sender: AnyObject)
   {
-  
+    if riderRequestActive == false
+    {
+      // save to Parse
+      
+      let riderRequest = PFObject(className: "riderRequest")
+      riderRequest["username"] = PFUser.currentUser()?.username
+      riderRequest["location"] = PFGeoPoint(latitude: latitude, longitude: longitude)
+      
+      // TO DO: check to make sure lat and long are not zero
+      
+      riderRequest.saveInBackgroundWithBlock {
+        (succeeded, error) -> Void in
+        if (succeeded)
+        {
+          self.callUberButton.setTitle("Cancel Uber", forState: UIControlState.Normal)
+          
+        } else {
+          
+          let alert = UIAlertController(title: "Could not call Uber", message: "Please try again!", preferredStyle: UIAlertControllerStyle.Alert)
+          alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+          self.presentViewController(alert, animated: true, completion: nil)
+        }
+      }
+      
+      riderRequestActive = true
+      
+    } else {
+      
+      self.callUberButton.setTitle("Call An Uber", forState: UIControlState.Normal)
+      
+      riderRequestActive = false
+      
+      // query
+      
+      let query = PFQuery(className:"riderRequest")
+      query.whereKey("username", equalTo: PFUser.currentUser()!.username!)
+      query.findObjectsInBackgroundWithBlock {
+        (objects: [AnyObject]?, error: NSError?) -> Void in
+        
+        if error == nil
+        {
+          print("Successfully retrieved \(objects!.count).")
+          
+          if let objects = objects as? [PFObject]
+          {
+            for object in objects
+            {
+              // delete location
+              
+              object.deleteInBackground()
+            }
+          }
+        } else {
+          
+          print(error)
+        }
+      }
+    }
   }
   
   func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
   {
     let location: CLLocationCoordinate2D = manager.location!.coordinate
+    self.latitude = location.latitude
+    self.longitude = location.longitude
     
     print("locations = \(location.latitude) \(location.longitude)")
     
